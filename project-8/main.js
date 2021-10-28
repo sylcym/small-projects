a8681f35f63aa3f5e76914e0667e35fb19d10665
sylcym <prusak.syl@gmail.com>
2021-10-28 19:32:14 +0200

    initial commit

---
+++ project-8/main.js
@@ -0,0 +1,15 @@
+'use strict';
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActive();
+    panel.classList.add('active');
+  });
+});
+
+function removeActive() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
